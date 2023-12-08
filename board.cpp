3d1755cfad1ea43f7bd9919c3277d4c223d4aa05
Luke Lyons <llyons4432@gmail.com>
2023-12-08 12:56:28 -0500

    reorganized some broadly useful functions into misc.hpp

--- board.cpp
+++ board.cpp
@@ -4,6 +4,7 @@
 #include <random>
 #include "board.hpp"
 #include "move_tables.hpp"
+#include "misc.hpp"
 
 /* Bitboard configuration
     29    30    31    32
@@ -32,37 +33,6 @@ Board::Board(){
 
 unsigned long long Board::get_all_pieces(){
    return red_bb | black_bb;
-}
-
-//converts a square index (according to the chart above) to its bitboard representation
-long long square_to_binary(const int square){
-   long long n = 0b1;
-   n = n << (square - 1);
-   return n;
-}
-
-//gets the square index of a binary number
-//NOTE: THERE CAN ONLY BE ONE 1 BIT
-int binary_to_square(const long long binary){
-   return log2(binary) + 1;
-}
-
-//gets the squares of all 1 bits in a bitboard
-std::vector<int> serialize_bb(long long bb){
-   std::vector<int> squares;
-   if(bb == 0){return squares;}
-
-   while (bb){
-      long long ls1b = bb & -bb; // isolate LS1B
-      squares.push_back(binary_to_square(ls1b)); //add squares to vector
-      bb &= bb-1; // reset LS1B
-   }
-   return squares;
-}
-
-//prints a binary representation of a number
-void print_binary(long long num){
-   std::cout << std::bitset<32>(num) << "\n";
 }
 
 //prints a representation of the board to the console
