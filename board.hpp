a241a6c0f6ccf3a6f95df1667a182e3c1e6496c7
Luke Lyons <llyons4432@gmail.com>
2024-01-01 03:21:55 -0500

    added comparison operator to Move class

--- board.hpp
+++ board.hpp
@@ -20,13 +20,16 @@ class Move{
         Move(long long r = 0, long long b = 0, long long k = 0, int c = 0, bool take = false);
         void get_move_info(const unsigned long long previous_pos);
         long long get_end_square(const unsigned long long previous_pos);
+
+        bool operator <(const Move& other) const{
+            return (reds + blacks + kings) < (other.reds + other.blacks + other.kings);
+        }
 };
 
 class Board{
 
     const unsigned long long red_promotion_mask = 0b11110000000000000000000000000000;
     const unsigned long long black_promotion_mask = 0b00000000000000000000000000001111;
-    bool has_takes;
     std::unordered_map<unsigned int, int> pos_history;
     const int repetition_limit = 3; //the number of times a position can be repeated before the game is considered a draw
 
@@ -34,6 +37,7 @@ class Board{
         unsigned long long red_bb;
         unsigned long long black_bb;
         unsigned long long king_bb;
+        bool has_takes;
         int movecount;
         int turn;
         Board();
